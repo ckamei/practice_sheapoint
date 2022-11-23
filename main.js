315a1c08ced06afbd435a2c93ad7d4eb635952d6
ckamei <kame.hnkmc106@gmail.com>
2022-11-23 16:37:42 +0900

    クリックしたらコピーはできる、けど一つずつのみ

--- main.js
+++ main.js
@@ -3,33 +3,22 @@ function changeText() {
   let name = document.getElementsByClassName('name')
   let url = '';
   let changeUrl = '';
-  let urlName = '';
   let url_regex = /"(.*?)"/g;
   for (let i = 0; i < name.length; i++) {
     url = name[i].innerText
   }
-
   for (let j = 0; j < 2; j++) {
     changeUrl = url.match(url_regex);
-    urlName = url.replace(url_regex, "")
-    console.log(changeUrl);
-    //console.log(urlName);
-    //name[0].innerHTML = `<a ${changeUrl}>${urlName}</a>`
     
+    console.log(changeUrl[j]);
+    
+    name[0].addEventListener('click', () => {
+      setTimeout(() => navigator.clipboard.writeText(changeUrl[1]), 100);
+      console.log('click');
+      alert("コピーできました！");
+    })
   }
-  name[0].addEventListener('click', () => {
-    setTimeout(() => navigator.clipboard.writeText(changeUrl[0]), 100);
-    console.log('click');
-    alert("コピーできました！");
-  })
 
 }
 
 changeText();
-
-// const button = document.querySelector('#button')
-// button.addEventListener('click', () => {
-//   navigator.clipboard.writeText('コピーされた文章')
-//   button.innerHTML = 'コピー完了！'
-//   setTimeout(() => button.innerHTML = '文章をコピー', 1000)
-// })
