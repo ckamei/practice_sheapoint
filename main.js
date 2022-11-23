ff1ced23107fb05cb5a4bdaae1d75a21a5e0d9dc
ckamei <kame.hnkmc106@gmail.com>
2022-11-23 17:47:55 +0900

    クリックしたら２つコピーできる、エラーなし

--- main.js
+++ main.js
@@ -7,27 +7,16 @@ function changeText() {
   for (let i = 0; i < name.length; i++) {
     url = name[i].innerText
   }
+
+  name[0].addEventListener('click', () => {
   for (let j = 0; j < 2; j++) {
     changeUrl = url.match(url_regex);
-    console.log(changeUrl[j]);
-    name[0].addEventListener('click', () => {
-      setTimeout(() => navigator.clipboard.writeText(changeUrl), 100);
-      console.log('hello');
-      //alert("コピーできました！");
-    })
   }
-    // passList += `<a href=${changeUrl[0]}>link</a>`
-    // name[0].insertAdjacentHTML("beforebegin", passList);
+  navigator.clipboard.writeText(changeUrl)
+    .then(() => alert("コピーできました！"));
+  })
 
 }
-//sampleArea.insertAdjacentHTML("beforebegin", "表示したい文字列");
+
 
 changeText();
-
-
-// onst button = document.querySelector('#button')
-// button.addEventListener('click', () => {
-//   navigator.clipboard.writeText('コピーされた文章')
-//   button.innerHTML = 'コピー完了！'
-//   setTimeout(() => button.innerHTML = '文章をコピー', 1000)
-// })
